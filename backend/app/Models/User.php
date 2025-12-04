feddcfcf09bc49e1e44bc2facd9073c1ff7ec998
Razanhroub <razan.b.alhroub@gmail.com>
2025-12-04 21:25:15 +0300

    migrations and relations

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -13,6 +13,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasRoles, SoftDeletes;
+    //used factory for the seeder
 
     /**
      * The attributes that are mass assignable.
@@ -35,6 +36,9 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+    /**
+     * Relation: the role assigned via spatie
+     */
     public function role()
     {
         return $this->belongsTo(\Spatie\Permission\Models\Role::class, 'role_id');
@@ -50,5 +54,33 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    /**
+     * Relation: if user is a teacher, has many classrooms
+     */
+    public function classrooms()
+    {
+        return $this->hasMany(Classroom::class, 'teacher_id');
+    }
+     /**
+     * Relation: if user is a student, has one student profile
+     */
+    public function studentProfile()
+    {
+        return $this->hasOne(Student::class, 'student_id');
+    }
+    /**
+     * Relation: teacher can get all students through their classrooms
+     */
+    public function students()
+    {
+        return $this->hasManyThrough(
+            Student::class,
+            Classroom::class,
+            'teacher_id',  // Foreign key on Classroom table
+            'class_id',    // Foreign key on Student table
+            'id',          // Local key on User table
+            'id'           // Local key on Classroom table
+        );
     }
 }
